9931475ca0099d8aa074a7de6d7f205be167a911
Magnús Skúlason <maggisk@gmail.com>
2018-04-29 20:49:26 +0000

    Maximum Subarray Sum

---
+++ maximum-subarray-sum.py
@@ -0,0 +1,15 @@
+#https://www.hackerrank.com/challenges/maximum-subarray-sum/problem
+# score 65 / 65 (hard)
+from bisect import bisect_right
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    sum_here = max_sum = 0
+    sorted_sums = [m]
+    for v in map(int, input().split()):
+        sum_here = (sum_here + v) % m
+        max_sum = max(max_sum, (m + sum_here - sorted_sums[bisect_right(sorted_sums, sum_here)]) % m)
+        i = bisect_right(sorted_sums, sum_here)
+        if i == 0 or sorted_sums[i - 1] != sum_here:
+            sorted_sums.insert(i, sum_here)
+    print(max_sum)
