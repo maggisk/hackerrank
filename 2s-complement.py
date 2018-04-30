80bb3e8cfc2f4e324e307adca4dd874c1f5a7480
Magnús Skúlason <maggisk@gmail.com>
2018-04-30 12:32:47 +0000

    simpler 2s-complement

--- 2s-complement.py
+++ 2s-complement.py
@@ -1,21 +1,17 @@
 # https://www.hackerrank.com/challenges/2s-complement/problem
 # score: 70 / 70
 
-def count_positive(n):
-    count = mul = 0
+def count(n):
+    if n < 0:
+        return 32 * (-n) - count(-n - 1)
+
+    cnt = mul = 0
     for i in range(31, -1, -1):
         b = 1 << i
         if b & n:
-            count += 1 + b * i // 2 + b * mul
+            cnt += 1 + b * i // 2 + b * mul
             mul += 1
-    return count
-
-def count_negative(n):
-    n = -n
-    return 32 * n - count_positive(n - 1)
-
-def count(n):
-    return count_positive(n) if n >= 0 else count_negative(n)
+    return cnt
 
 for _ in range(int(input())):
     a, b = map(int, input().split())
